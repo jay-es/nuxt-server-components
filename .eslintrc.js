38bd9ac34d5c47c8c794a81b252b409c47b6e3ae
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-09-15 20:32:31 +0900

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,38 +5,57 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/strict',
-    'plugin:@typescript-eslint/stylistic',
-    'prettier',
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
+  extends: ['eslint:recommended'],
   plugins: ['simple-import-sort'],
   rules: {
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/consistent-type-imports': 'error', // tsconfig の verbatimModuleSyntax の代わり
-    '@typescript-eslint/no-import-type-side-effects': 'error', // tsconfig の verbatimModuleSyntax の代わり
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
   },
   overrides: [
     {
-      files: '*.vue',
+      files: ['*.ts', '*.tsx', '*.vue'],
+      extends: [
+        'plugin:@typescript-eslint/strict',
+        'plugin:@typescript-eslint/stylistic',
+      ],
       rules: {
-        'no-undef': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/consistent-type-imports': 'error', // tsconfig の verbatimModuleSyntax の代わり
+        '@typescript-eslint/no-import-type-side-effects': 'error', // tsconfig の verbatimModuleSyntax の代わり
+      },
+    },
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+      },
+    },
+    {
+      // Vue
+      files: '*.vue',
+      extends: ['plugin:vue/vue3-recommended'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        ...require('@typescript-eslint/eslint-plugin').configs[
+          'eslint-recommended'
+        ].overrides[0].rules,
         'vue/no-v-html': 'off',
       },
     },
     {
-      files: 'pages/**/*.vue',
+      // Nuxt page components
+      files: 'pages/**/[a-z]*.vue',
       rules: {
         'vue/multi-word-component-names': 'off',
       },
+    },
+    {
+      // Prettier
+      files: ['*.*'],
+      extends: ['prettier'],
     },
   ],
 };
